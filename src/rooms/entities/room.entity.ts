a78472d04d0d4b5c4d6e7dd7695f0ca47270cfbf
ZakariaRaslan <zakariaraslan4@gmail.com>
2024-01-16 17:56:23 +0200

    updates

--- src/rooms/entities/room.entity.ts
+++ src/rooms/entities/room.entity.ts
@@ -1,10 +1,10 @@
 export class Room {
-    roomNumber: string;
-    roomType: string;
-    amenities: string;
-    price: number;
-    photos: string;
-    checkinTime: Date;
-    checkoutTime: Date;
-    rating: number;
+  roomNumber: string;
+  roomType: string;
+  amenities: string;
+  price: number;
+  photos: string;
+  checkinTime: Date;
+  checkoutTime: Date;
+  rating: number;
 }
